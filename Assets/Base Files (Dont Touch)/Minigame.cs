c5f3c78adbd430928e963d24e0bcd27aa1bd20a7
texas-egads <36241304+texas-egads@users.noreply.github.com>
2021-12-14 12:54:15 -0600

    Initial commit

---
+++ Assets/Base Files (Dont Touch)/Minigame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Minigame")]
+public class Minigame : ScriptableObject
+{    
+        public enum GameTime
+        {
+            Short,
+            Long
+        }
+        //******* ADD THESE IN THE INSPECTOR *******//
+        public GameTime gameTime;
+        public AudioClip music;
+
+        public SoundAsset[] sounds;
+
+        //*****************************************//
+
+        //******* UPDATE THIS WHEN THE PLAYER WINS *******//
+        public bool gameWin;
+        //***********************************************//
+}
